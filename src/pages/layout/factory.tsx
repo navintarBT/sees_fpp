d13c174bff7ce24b1314f1f84859132948c0ebef
Rattanaxay <rattanaxay.k@gmail.com>
2026-04-30 14:26:59 +0700

    tar

--- src/pages/layout/factory.tsx
+++ src/pages/layout/factory.tsx
@@ -7,8 +7,8 @@ const Factory = () => {
     { label: '入荷実績登録機能', path: '', className: 'mockup-red' },
     { label: '入荷工程登録', path: '', className: 'mockup-blue' },
     { label: '棚移動', path: '', className: 'mockup-green' },
-    { label: '作業オーダー部品払出', path: '/factory/button-access', className: 'mockup-yellow' },
-    { label: '作業オーダー実績時間登録', path: '/factory/work-order-time-registration', className: 'mockup-gray' },
+    { label: '作業オーダー\n部品払出', path: '/factory/button-access', className: 'mockup-yellow' },
+    { label: '作業オーダー\n実績時間登録', path: '/factory/work-order-time-registration', className: 'mockup-gray' },
     { label: '作業オーダー完了', path: '/factory/work-order-completion', className: 'mockup-orange' },
     { label: '出荷実績登録', path: '', className: 'mockup-pink' },
 
@@ -29,7 +29,12 @@ const Factory = () => {
 
             <div className='mockup-grid'>
               {factoryButtons.map((btn) => (
-                <button key={btn.label} className={`mockup-btn ${btn.className}`} onClick={() => navigate(btn.path)}>
+                <button
+                  key={btn.label}
+                  className={`mockup-btn ${btn.className}`}
+                  onClick={() => navigate(btn.path)}
+                  style={{ whiteSpace: 'pre-line' }}
+                >
                   {btn.label}
                 </button>
               ))}
