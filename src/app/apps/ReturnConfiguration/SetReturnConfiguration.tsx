ca02444a16f2a8a0988731ca459297c6c1584e5d
Kongchan <kongchauh@gmail.com>
2026-05-25 10:22:33 +0700

    update new update

--- src/app/apps/ReturnConfiguration/SetReturnConfiguration.tsx
+++ src/app/apps/ReturnConfiguration/SetReturnConfiguration.tsx
@@ -573,7 +573,7 @@ const SetReturnConfiguration = () => {
             <div className='set-modal-backdrop' role='presentation'>
               <div className='set-modal' role='dialog' aria-modal='true'>
                 <div className='set-modal-header'>確認</div>
-                <div className='set-modal-body'>選択した行を削除します。<br />よろしいですか？</div>
+                <div className='set-modal-body'>選択行を削除しますか？</div>
                 <div className='set-modal-actions'>
                   <button
                     className='set-modal-btn set-modal-yes'
